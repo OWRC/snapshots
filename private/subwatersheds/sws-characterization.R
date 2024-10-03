49a9d7db130e326274a98b91b33339770dc6ecb3
maseology <maseology@pm.me>
2024-10-03 15:46:50 -0400

    updated to v2023

--- private/subwatersheds/sws-characterization.R
+++ private/subwatersheds/sws-characterization.R
@@ -1,22 +1,22 @@
 library(leaflet)
 library(leaflet.extras)
 library(dplyr)
-library(rgdal)
+library(sf)
 
 
-sws.ntwrk <- readLines("https://www.dropbox.com/s/cmzzownz046djij/owrc20-50a_SWS10_ntwrk.geojson?dl=1") %>% paste(collapse = "\n")
-geojson <- readOGR("https://www.dropbox.com/s/ro16gg6zi4kqbc0/owrc20-50a_SWS10-final.geojson?dl=1",verbose = FALSE)
+# sws.ntwrk <- readLines("https://www.dropbox.com/s/cmzzownz046djij/owrc20-50a_SWS10_ntwrk.geojson?dl=1") %>% paste(collapse = "\n")
+geojson <- read_sf("E:/Sync/@owrc/pages/interpolants/interpolation/calc/landuse/output/PDEM-South-D2013-OWRC23-60-HC-sws10-simpl.geojson")
 geojson$iperc <- as.numeric(factor(geojson$perm))
 
 m <- leaflet(geojson) %>%
-  addProviderTiles( providers$Stamen.TonerLite, options = providerTileOptions(noWrap = TRUE) ) %>%
+  addTiles() %>%
   addFullscreenControl() %>%
   addPolygons(color="black",
               fillColor = ~colorQuantile("Purples", geojson$perimp)(perimp),
               fillOpacity = 1,
               weight = 2,
-              popup = ~paste0('<b>sub-watershed: ',mmID,'</b>',
-                              '<br>             area: ',round(Area/1000000,1),'km²',
+              popup = ~paste0('<b>sub-watershed: ',swsid,'</b>',
+                              '<br>             area: ',round(area,1),'km²',
                               '<br>     permeability: ',perm,
                               '<br> impervious cover: ',round(perimp*100,0),'%',
                               '<br>     canopy cover: ',round(percov*100,0),'%',
@@ -32,8 +32,8 @@ m <- leaflet(geojson) %>%
               fillColor = ~colorQuantile("YlGn", geojson$percov)(percov),
               fillOpacity = 1,
               weight = 2,
-              popup = ~paste0('<b>sub-watershed: ',mmID,'</b>',
-                              '<br>             area: ',round(Area/1000000,1),'km²',
+              popup = ~paste0('<b>sub-watershed: ',swsid,'</b>',
+                              '<br>             area: ',round(area,1),'km²',
                               '<br>     permeability: ',perm,
                               '<br> impervious cover: ',round(perimp*100,0),'%',
                               '<br>     canopy cover: ',round(percov*100,0),'%',
@@ -49,8 +49,8 @@ m <- leaflet(geojson) %>%
               fillColor = ~colorQuantile("Blues", geojson$perow)(perow),
               fillOpacity = 1,
               weight = 2,
-              popup = ~paste0('<b>sub-watershed: ',mmID,'</b>',
-                              '<br>             area: ',round(Area/1000000,1),'km²',
+              popup = ~paste0('<b>sub-watershed: ',swsid,'</b>',
+                              '<br>             area: ',round(area,1),'km²',
                               '<br>     permeability: ',perm,
                               '<br> impervious cover: ',round(perimp*100,0),'%',
                               '<br>     canopy cover: ',round(percov*100,0),'%',
@@ -66,8 +66,8 @@ m <- leaflet(geojson) %>%
               fillColor = ~colorQuantile("Oranges", geojson$perwl)(perwl),
               fillOpacity = 1,
               weight = 2,
-              popup = ~paste0('<b>sub-watershed: ',mmID,'</b>',
-                              '<br>             area: ',round(Area/1000000,1),'km²',
+              popup = ~paste0('<b>sub-watershed: ',swsid,'</b>',
+                              '<br>             area: ',round(area,1),'km²',
                               '<br>     permeability: ',perm,
                               '<br> impervious cover: ',round(perimp*100,0),'%',
                               '<br>     canopy cover: ',round(percov*100,0),'%',
@@ -83,8 +83,8 @@ m <- leaflet(geojson) %>%
               fillColor = ~colorFactor("Dark2", geojson$iperc)(iperc),
               fillOpacity = 1,
               weight = 2,
-              popup = ~paste0('<b>sub-watershed: ',mmID,'</b>',
-                              '<br>             area: ',round(Area/1000000,1),'km²',
+              popup = ~paste0('<b>sub-watershed: ',swsid,'</b>',
+                              '<br>             area: ',round(area,1),'km²',
                               '<br>     permeability: ',perm,
                               '<br> impervious cover: ',round(perimp*100,0),'%',
                               '<br>     canopy cover: ',round(percov*100,0),'%',
@@ -96,13 +96,14 @@ m <- leaflet(geojson) %>%
                 sendToBack = FALSE),
               group = "Relative permeability"              
   ) %>%
-  addGeoJSON(sws.ntwrk, weight = 3, color = "brown", opacity=1, group = "connectivity") %>%
+  # addGeoJSON(sws.ntwrk, weight = 3, color = "brown", opacity=1, group = "connectivity") %>%
   addLayersControl(
-    overlayGroups = c("connectivity"),
+    # overlayGroups = c("connectivity"),
     baseGroups = c("Imperviousness", "Tree cover","Wetland cover","Open water cover","Relative permeability"),
     options = layersControlOptions(collapsed = FALSE)
-  ) %>% hideGroup("connectivity") %>%
+  ) %>% 
+  # hideGroup("connectivity") %>%
   setView(lng = -78.7, lat = 44.2, zoom = 8)
 
 
-htmlwidgets::saveWidget(m, file="subwatersheds/sws-characterization.html")
+htmlwidgets::saveWidget(m, file="sws-characterization.html")
